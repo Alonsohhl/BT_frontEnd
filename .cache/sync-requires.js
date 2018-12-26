2e476871e1112b6d01a0c6ee4ec352855149d311
Alonso Hector Huayta Laura <aHuayta@panam.com.pe>
2018-12-26 17:51:15 -0500

    poco

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -3,12 +3,12 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("E:\\Sites\\BT3\\.cache\\dev-404-page.js")),
-  "component---src-pages-404-js": preferDefault(require("E:\\Sites\\BT3\\src\\pages\\404.js")),
-  "component---src-pages-index-js": preferDefault(require("E:\\Sites\\BT3\\src\\pages\\index.js")),
-  "component---src-pages-login-js": preferDefault(require("E:\\Sites\\BT3\\src\\pages\\login.js")),
-  "component---src-pages-page-2-js": preferDefault(require("E:\\Sites\\BT3\\src\\pages\\page-2.js")),
-  "component---src-pages-page-3-js": preferDefault(require("E:\\Sites\\BT3\\src\\pages\\page-3.js")),
-  "component---src-pages-page-4-js": preferDefault(require("E:\\Sites\\BT3\\src\\pages\\page-4.js"))
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Sites\\BT_frontEnd\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\Sites\\BT_frontEnd\\src\\pages\\404.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Sites\\BT_frontEnd\\src\\pages\\index.js")),
+  "component---src-pages-login-js": preferDefault(require("C:\\Sites\\BT_frontEnd\\src\\pages\\login.js")),
+  "component---src-pages-page-2-js": preferDefault(require("C:\\Sites\\BT_frontEnd\\src\\pages\\page-2.js")),
+  "component---src-pages-page-3-js": preferDefault(require("C:\\Sites\\BT_frontEnd\\src\\pages\\page-3.js")),
+  "component---src-pages-page-4-js": preferDefault(require("C:\\Sites\\BT_frontEnd\\src\\pages\\page-4.js"))
 }
 
--- src/pages/login.js
+++ src/pages/login.js
@@ -4,6 +4,8 @@ import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Footer from '../components/footer'
 
+
+//http://jasonwatmore.com/post/2017/09/16/react-redux-user-registration-and-login-tutorial-example
 const LoginPage = () => (
     <Layout>
         <div>
@@ -13,14 +15,9 @@ const LoginPage = () => (
           <div className="container">
             <div className="row">
               <div className="col-md-12">
-                <h2>Log In</h2>
+                <h2>Login</h2>
                 {/* Breadcrumbs */}
-                <nav id="breadcrumbs" className="dark">
-                  <ul>
-                    <li><a href="#">Home</a></li>
-                    <li>Log In</li>
-                  </ul>
-                </nav>
+                
               </div>
             </div>
           </div>
@@ -33,36 +30,39 @@ const LoginPage = () => (
               <div className="login-register-page">
                 {/* Welcome Text */}
                 <div className="welcome-text">
-                  <h3>We're glad to see you again!</h3>
-                  <span>Don't have an account? <a href="pages-register.html">Sign Up!</a></span>
+                  <h3>Acceder</h3>
+                  <span>¿No tienes una cuenta? 
+                  <Link to="/registro" className="current"> Registrate!</Link>
+                    </span>
                 </div>
                 {/* Form */}
                 <form method="post" id="login-form">
                   <div className="input-with-icon-left">
                     <i className="icon-material-baseline-mail-outline" />
-                    <input type="text" className="input-text with-border" name="emailaddress" id="emailaddress" placeholder="Email Address" required />
+                    <input type="text" className="input-text with-border" name="emailaddress" id="emailaddress" placeholder="Correo electrónico" required />
                   </div>
                   <div className="input-with-icon-left">
                     <i className="icon-material-outline-lock" />
-                    <input type="password" className="input-text with-border" name="password" id="password" placeholder="Password" required />
+                    <input type="password" className="input-text with-border" name="password" id="password" placeholder="Contraseña" required />
                   </div>
-                  <a href="#" className="forgot-password">Forgot Password?</a>
+                  <a href="#" className="forgot-password">¿Olvidaste la contraseña?</a>
                 </form>
                 {/* Button */}
-                <button className="button full-width button-sliding-icon ripple-effect margin-top-10" type="submit" form="login-form">Log In <i className="icon-material-outline-arrow-right-alt" /></button>
-                {/* Social Login */}
+                <button className="button full-width button-sliding-icon ripple-effect margin-top-10" type="submit" form="login-form">Acceder <i className="icon-material-outline-arrow-right-alt" /></button>
+                {/* Social Login
                 <div className="social-login-separator"><span>or</span></div>
                 <div className="social-login-buttons">
                   <button className="facebook-login ripple-effect"><i className="icon-brand-facebook-f" /> Log In via Facebook</button>
                   <button className="google-login ripple-effect"><i className="icon-brand-google-plus-g" /> Log In via Google+</button>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="margin-top-70" />
+      <div className="margin-top-200"/>
+        
       
       
   <Footer/>
